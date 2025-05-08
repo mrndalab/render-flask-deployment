cf966ddb77241bc4e0e756fcefe9b867de973bc4
mrndalab <akshatatrbkriti@gmail.com>
2025-05-08 18:44:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from nsedata import eq_opt_ltp
 
 app = Flask(__name__)
 
-my_variable=eq_opt_ltp('WIPR0', 230, '29-May-2025', 'PE')
+my_variable=eq_opt_ltp("WIPR0", 230, '29-May-2025', 'PE')
 
 html_content = """
     <!DOCTYPE html>
